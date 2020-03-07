a7c3806c06ef9878e206cd86bf3a979e303b27b2
shevaukr07 <61219381+shevaukr07@users.noreply.github.com>
2020-03-07 23:01:08 +0900

    commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){ 
-  var last_message_id = $('.message:last').data("message-id");
   function buildHTML(message){
-   if ( message.image ) {
+   if  (message.content && message.image) {
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -19,8 +18,7 @@ $(function(){
          </div>
          <img src=${message.image} >
        </div>`
-     return html;
-   } else {
+   } else if (message.content) {
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -37,13 +35,29 @@ $(function(){
            </p>
          </div>
        </div>`
-     return html;
+   } else if (message.image) {
+    var html = `<div class="message" data-message-id=  ${message.id}  >
+       <div class="upper-message">
+         <div class="upper-message__user-name">
+          ${message.user_name}
+         </div>
+         <div class="upper-message__date">
+          ${message.date}
+         </div>
+       </div>
+       <div class="lower-message"> 
+         <img src=" ${message.image} " class="lower-message__image" >
+       </div> 
+     </div>`
    };
- }
+   return html;
+  };
+ 
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
+
  $.ajax({
    url: url,
    type: "POST",
@@ -63,4 +77,30 @@ $('#new_message').on('submit', function(e){
    });
    return false;
  });
+
+ var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.messages').append(insertHTML); 
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+   }
+  })
+  .fail(function() {
+    alert('error');
+  });
+ };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+   setInterval(reloadMessages, 7000);
+ }
 });
